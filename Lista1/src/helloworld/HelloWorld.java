0c04f6f383242cade1b088380de2df8d121c42e3
Paulo César <oliveira.pc98@gmail.com>
2017-10-21 11:39:13 -0200

    Criação arquivo Hello World

---
+++ Lista1/src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+
+package helloworld;
+
+/**
+ *
+ * @author Paulo César
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
